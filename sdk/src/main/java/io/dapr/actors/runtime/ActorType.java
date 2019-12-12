882e44a53397bdcad9dd7ff79517ac35ff80207e
Artur Souza <artursouza.ms@outlook.com>
2019-12-11 16:42:56 -0800

    ActorTypeInformation with parsing logic + some stubs.

---
+++ sdk/src/main/java/io/dapr/actors/runtime/ActorType.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ */
+
+package io.dapr.actors.runtime;
+
+import java.lang.annotation.*;
+
+/**
+ * Annotation to override default behavior of Actor class.
+ */
+@Documented
+@Target(ElementType.TYPE_USE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ActorType {
+
+    String Name();
+
+}
---
+++ sdk/src/main/java/io/dapr/actors/runtime/Remindable.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ */
+
+package io.dapr.actors.runtime;
+
+/**
+ * TODO
+ */
+public interface Remindable {
+}
