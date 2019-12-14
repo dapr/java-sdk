bb0463fe57f7501aac71ed5e3b124ce5b9fb49f2
LM <lemai>
2019-12-13 19:27:10 -0800

    Add ActorMethodInfoMap

---
+++ sdk/src/main/java/io/dapr/actors/runtime/ActorMethodInfoMap.java
@@ -0,0 +1,32 @@
+package io.dapr.actors.runtime;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
+class ActorMethodInfoMap {
+    private final HashMap<String, Method> methods;
+
+    public <T> ActorMethodInfoMap(Iterable<Class<T>> interfaceTypes)
+    {
+        this.methods = new HashMap<String, Method>();
+
+        // Find methods which are defined in Actor interface.
+        for (Class<T> actorInterface : interfaceTypes)
+        {
+            for (Method methodInfo : actorInterface.getMethods())
+            {
+                this.methods.put(methodInfo.getName(), methodInfo);
+            }
+        }
+    }
+
+    public Method LookupActorMethodInfo(String methodName) throws NoSuchMethodException
+    {
+        Method methodInfo = this.methods.get(methodName);
+        if (methodInfo == null) {
+            throw new NoSuchMethodException("Actor type doesn't contain method " + methodName);
+        }
+
+        return methodInfo;
+    }
+}
