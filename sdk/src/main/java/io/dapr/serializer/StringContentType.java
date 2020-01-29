c6cecc56b691fd73576ec8aeab6ca1d4fb54f8c3
LMWF <lemai@microsoft.com>
2020-01-29 14:09:52 -0800

    checkstyle

--- sdk/src/main/java/io/dapr/serializer/StringContentType.java
+++ sdk/src/main/java/io/dapr/serializer/StringContentType.java
@@ -5,11 +5,13 @@
 
 package io.dapr.serializer;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
  * Flags a serializer indicating that byte[] contains String for both input and output.
- *
  * This information can be used to at the state store, for example, to save serialized data as plain text.
  */
 @Documented
