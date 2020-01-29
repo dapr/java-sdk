e72f1fa9196d84cafa84bb1d8b2c52b492a4b1e3
Leon Mai <lemai@microsoft.com>
2020-01-29 15:17:33 -0800

    Actor IT - timers, reminders, turn-based concurrency (#162)
    * Actor IT - timers, reminders, turn-based concurrency
    
    IT for actor timer, reminder, turn-based concurrency
    
    * Remove commented out code
    
    * Remove comment
    
    * Update MyActorService.java
    
    * Update ActorTurnBasedConcurrencyIT.java
    
    * Keep the latest stable version of junit. (#169)
    
    * merge with changes in master
    
    Co-authored-by: Artur Souza <artursouza.ms@outlook.com>

---
+++ sdk-tests/src/test/java/io/dapr/it/actors/app/MyActorService.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ */
+
+package io.dapr.it.actors.app;
+
+import io.dapr.actors.runtime.ActorRuntime;
+
+public class MyActorService {
+  public static final String SUCCESS_MESSAGE = "actors: established connection to placement service at localhost";
+
+  /**
+   * Starts the service.
+   * @param args Expects the port: -p PORT
+   * @throws Exception If cannot start service.
+   */
+  public static void main(String[] args) throws Exception {
+    System.out.println("Hello from main() MyActorService");
+
+    long port = Long.parseLong(args[0]);
+    ActorRuntime.getInstance().registerActor(MyActorImpl.class);
+
+    TestApplication.start(port);
+  }
+}
