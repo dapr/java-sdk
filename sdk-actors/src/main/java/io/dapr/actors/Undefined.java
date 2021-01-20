446cd17e186e314017711ed4934299de30ff087a
Artur Souza <artursouza.ms@outlook.com>
2021-01-19 23:34:14 -0800

    Support ActorMethod to change method's name. (#448)

---
+++ sdk-actors/src/main/java/io/dapr/actors/Undefined.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ */
+
+package io.dapr.actors;
+
+/**
+ * Internal class to represent the undefined value for an optional Class attribute.
+ */
+final class Undefined {
+
+  private Undefined() {
+  }
+
+}
