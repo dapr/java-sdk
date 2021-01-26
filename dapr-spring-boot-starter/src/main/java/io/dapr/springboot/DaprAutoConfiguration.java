0deedd4cc1a712ce715df40e7f130c685afe17cf
Darren Wang <darren_wang@outlook.com>
2021-01-26 13:15:58 +0800

    Adding Dapr Spring Boot Starter.

---
+++ dapr-spring-boot-starter/src/main/java/io/dapr/springboot/DaprAutoConfiguration.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ */
+
+package io.dapr.springboot;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Dapr's Spring Boot AutoConfiguration.
+ */
+@Configuration
+@ConditionalOnWebApplication
+@ComponentScan("io.dapr.springboot")
+public class DaprAutoConfiguration {
+}
