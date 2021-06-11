8bec8714d7dd0d2f5defc55e78d2b4c336abd5b0
Artur Souza <artursouza.ms@outlook.com>
2021-06-10 23:26:30 -0700

    Change Actor gRPC client to async.

--- sdk-actors/src/main/java/io/dapr/actors/client/DaprGrpcClient.java
+++ sdk-actors/src/main/java/io/dapr/actors/client/DaprGrpcClient.java
@@ -5,12 +5,26 @@
 
 package io.dapr.actors.client;
 
-import com.google.common.util.concurrent.ListenableFuture;
 import com.google.protobuf.ByteString;
+import io.dapr.config.Properties;
 import io.dapr.exceptions.DaprException;
+import io.dapr.internal.opencensus.GrpcWrapper;
 import io.dapr.v1.DaprGrpc;
 import io.dapr.v1.DaprProtos;
+import io.grpc.CallOptions;
+import io.grpc.Channel;
+import io.grpc.ClientCall;
+import io.grpc.ClientInterceptor;
+import io.grpc.ForwardingClientCall;
+import io.grpc.Metadata;
+import io.grpc.MethodDescriptor;
+import io.grpc.stub.StreamObserver;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.MonoSink;
+import reactor.util.context.Context;
+
+import java.util.concurrent.ExecutionException;
+import java.util.function.Consumer;
 
 /**
  * A DaprClient over GRPC for Actor.
@@ -18,19 +32,17 @@ import reactor.core.publisher.Mono;
 class DaprGrpcClient implements DaprClient {
 
   /**
-   * The GRPC client to be used.
-   *
-   * @see DaprGrpc.DaprFutureStub
+   * The async gRPC stub.
    */
-  private DaprGrpc.DaprFutureStub client;
+  private DaprGrpc.DaprStub client;
 
   /**
    * Internal constructor.
    *
    * @param grpcClient Dapr's GRPC client.
    */
-  DaprGrpcClient(DaprGrpc.DaprFutureStub grpcClient) {
-    this.client = grpcClient;
+  DaprGrpcClient(DaprGrpc.DaprStub grpcClient) {
+    this.client = intercept(grpcClient);
   }
 
   /**
@@ -38,26 +50,81 @@ class DaprGrpcClient implements DaprClient {
    */
   @Override
   public Mono<byte[]> invoke(String actorType, String actorId, String methodName, byte[] jsonPayload) {
-    return Mono.fromCallable(DaprException.wrap(() -> {
-      DaprProtos.InvokeActorRequest req =
-          DaprProtos.InvokeActorRequest.newBuilder()
-              .setActorType(actorType)
-              .setActorId(actorId)
-              .setMethod(methodName)
-              .setData(jsonPayload == null ? ByteString.EMPTY : ByteString.copyFrom(jsonPayload))
-              .build();
-
-      return get(client.invokeActor(req));
-    })).map(r -> r.getData().toByteArray());
+    DaprProtos.InvokeActorRequest req =
+        DaprProtos.InvokeActorRequest.newBuilder()
+            .setActorType(actorType)
+            .setActorId(actorId)
+            .setMethod(methodName)
+            .setData(jsonPayload == null ? ByteString.EMPTY : ByteString.copyFrom(jsonPayload))
+            .build();
+    return Mono.subscriberContext().flatMap(
+        context -> this.<DaprProtos.InvokeActorResponse>createMono(
+            it -> intercept(context, client).invokeActor(req, it)
+        )
+    ).map(r -> r.getData().toByteArray());
   }
 
-  private static <V> V get(ListenableFuture<V> future) {
-    try {
-      return future.get();
-    } catch (Exception e) {
-      DaprException.wrap(e);
-    }
+  /**
+   * Populates GRPC client with interceptors.
+   *
+   * @param client GRPC client for Dapr.
+   * @return Client after adding interceptors.
+   */
+  private static DaprGrpc.DaprStub intercept(DaprGrpc.DaprStub client) {
+    ClientInterceptor interceptor = new ClientInterceptor() {
+      @Override
+      public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(
+          MethodDescriptor<ReqT, RespT> methodDescriptor,
+          CallOptions callOptions,
+          Channel channel) {
+        ClientCall<ReqT, RespT> clientCall = channel.newCall(methodDescriptor, callOptions);
+        return new ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(clientCall) {
+          @Override
+          public void start(final Listener<RespT> responseListener, final Metadata metadata) {
+            String daprApiToken = Properties.API_TOKEN.get();
+            if (daprApiToken != null) {
+              metadata.put(Metadata.Key.of("dapr-api-token", Metadata.ASCII_STRING_MARSHALLER), daprApiToken);
+            }
 
-    return null;
+            super.start(responseListener, metadata);
+          }
+        };
+      }
+    };
+    return client.withInterceptors(interceptor);
+  }
+
+  /**
+   * Populates GRPC client with interceptors for telemetry.
+   *
+   * @param context Reactor's context.
+   * @param client GRPC client for Dapr.
+   * @return Client after adding interceptors.
+   */
+  private static DaprGrpc.DaprStub intercept(Context context, DaprGrpc.DaprStub client) {
+    return GrpcWrapper.intercept(context, client);
+  }
+
+  private <T> Mono<T> createMono(Consumer<StreamObserver<T>> consumer) {
+    return Mono.create(sink -> DaprException.wrap(() -> consumer.accept(createStreamObserver(sink))).run());
+  }
+
+  private <T> StreamObserver<T> createStreamObserver(MonoSink<T> sink) {
+    return new StreamObserver<T>() {
+      @Override
+      public void onNext(T value) {
+        sink.success(value);
+      }
+
+      @Override
+      public void onError(Throwable t) {
+        sink.error(DaprException.propagate(new ExecutionException(t)));
+      }
+
+      @Override
+      public void onCompleted() {
+        sink.success();
+      }
+    };
   }
 }
