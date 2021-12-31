276debce9956bc82e0a1badb6829df4d1918fbd5
pravinpushkar <ppushkar@microsoft.com>
2021-12-31 23:41:08 +0530

    Copyright changes
    Signed-off-by: pravinpushkar <ppushkar@microsoft.com>

--- sdk/src/main/java/io/dapr/client/DaprPreviewClient.java
+++ sdk/src/main/java/io/dapr/client/DaprPreviewClient.java
@@ -1,7 +1,15 @@
 /*
- * Copyright (c) Microsoft Corporation and Dapr Contributors.
- * Licensed under the MIT License.
- */
+ * Copyright 2021 The Dapr Authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 package io.dapr.client;
 
@@ -127,7 +135,7 @@ public interface DaprPreviewClient extends AutoCloseable {
    *
    * @param storeName Name of the configuration store
    * @param keys      keys of the configurations which are to be subscribed
-   * @param metadata optional metadata
+   * @param metadata  optional metadata
    * @return Flux of List of configuration items
    */
   Flux<List<ConfigurationItem>> subscribeToConfigurations(String storeName, List<String> keys,
