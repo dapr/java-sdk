1595ff75e4bba2e8f1c127214074af53546a9446
Mukundan Sundararajan <msundar.ms@outlook.com>
2022-03-15 10:23:12 +0530

    remove deprecated classes
    Signed-off-by: Mukundan Sundararajan <msundar.ms@outlook.com>

--- examples/src/main/java/io/dapr/examples/tracing/InvokeClient.java
+++ examples/src/main/java/io/dapr/examples/tracing/InvokeClient.java
@@ -17,7 +17,6 @@ import io.dapr.client.DaprClient;
 import io.dapr.client.DaprClientBuilder;
 import io.dapr.client.domain.HttpExtension;
 import io.dapr.client.domain.InvokeMethodRequest;
-import io.dapr.client.domain.InvokeMethodRequestBuilder;
 import io.dapr.examples.OpenTelemetryConfig;
 import io.dapr.utils.TypeRef;
 import io.opentelemetry.api.OpenTelemetry;
