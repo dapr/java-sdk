a448542ee90fac772dd38f69a91ee63817b9f7c3
Shubham Sharma <shubhash@microsoft.com>
2023-01-13 13:52:09 +0530

    Fix licenses
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

--- sdk/src/test/java/io/dapr/client/domain/BulkMessageTest.java
+++ sdk/src/test/java/io/dapr/client/domain/BulkMessageTest.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2023 The Dapr Authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package io.dapr.client.domain;
 
 import org.junit.Test;
@@ -11,7 +24,7 @@ import java.util.Map;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
-public class DaprBulkMessageTest {
+public class BulkMessageTest {
   @Test
   public void testSetMetadata() {
     // Arrange
