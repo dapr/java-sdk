6d12400aab6e8e398c0509493ecf58809e4007c5
MregXN <mregxn@gmail.com>
2023-06-25 15:16:40 +0800

    original version of service invocation grpc proxying
    Signed-off-by: MregXN <mregxn@gmail.com>

--- examples/src/main/java/io/dapr/examples/invoke/grpc/HelloWorldClient.java
+++ examples/src/main/java/io/dapr/examples/invoke/grpc/HelloWorldClient.java
@@ -13,18 +13,34 @@ limitations under the License.
 
 package io.dapr.examples.invoke.grpc;
 
+import java.util.concurrent.TimeUnit;
+
 import io.dapr.client.DaprClient;
 import io.dapr.client.DaprClientBuilder;
 import io.dapr.client.domain.HttpExtension;
+import io.dapr.examples.DaprExamplesProtos.HelloReply;
+import io.dapr.examples.DaprExamplesProtos.HelloRequest;
+import io.dapr.examples.HelloWorldGrpc;
+import io.grpc.Grpc;
+import io.grpc.InsecureChannelCredentials;
+import io.grpc.ManagedChannel;
+import io.grpc.Metadata;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.MetadataUtils;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * 1. Build and install jars:
  * mvn clean install
  * 2. cd [repo root]/examples
  * 2. Send messages to the server:
- * dapr run -- java -jar target/dapr-java-sdk-examples-exec.jar io.dapr.examples.invoke.grpc.HelloWorldClient
+ * dapr run -- java -jar target/dapr-java-sdk-examples-exec.jar
+ * io.dapr.examples.invoke.grpc.HelloWorldClient
  */
 public class HelloWorldClient {
+
+  private static final Logger logger = Logger.getLogger(HelloWorldClient.class.getName());
 
   /**
    * The main method of the client app.
@@ -32,24 +48,51 @@ public class HelloWorldClient {
    * @param args Array of messages to be sent.
    */
   public static void main(String[] args) throws Exception {
-    try (DaprClient client = new DaprClientBuilder().build()) {
 
-      String serviceAppId = "hellogrpc";
-      String method = "say";
-
-      int count = 0;
-      while (true) {
-        String message = "Message #" + (count++);
-        System.out.println("Sending message: " + message);
-        client.invokeMethod(serviceAppId, method, message, HttpExtension.NONE).block();
-        System.out.println("Message sent: " + message);
-
-        Thread.sleep(1000);
-
-        // This is an example, so for simplicity we are just exiting here.
-        // Normally a dapr app would be a web service and not exit main.
-        System.out.println("Done");
+    String user = "world";
+    // Access a service running on the local machine on port 50051
+    String target = "localhost:50051";
+    // Allow passing in the user and target strings as command line arguments
+    if (args.length > 0) {
+      if ("--help".equals(args[0])) {
+        System.err.println("Usage: [name [target]]");
+        System.err.println("");
+        System.err.println("  name    The name you wish to be greeted by. Defaults to " + user);
+        System.err.println("  target  The server to connect to. Defaults to " + target);
+        System.exit(1);
       }
+      user = args[0];
     }
+    if (args.length > 1) {
+      target = args[1];
+    }
+    ManagedChannel channel = Grpc.newChannelBuilder(target, InsecureChannelCredentials.create())
+        .build();
+
+    try {
+      HelloWorldGrpc.HelloWorldBlockingStub blockingStub = HelloWorldGrpc.newBlockingStub(channel);
+
+      Metadata headers = new Metadata();
+      headers.put(Metadata.Key.of("dapr-app-id", Metadata.ASCII_STRING_MARSHALLER),
+          "hellogrpc");
+      blockingStub = MetadataUtils.attachHeaders(blockingStub, headers);
+
+      logger.info("Will try to greet " + user + " ...");
+      try {
+        HelloRequest request = HelloRequest.newBuilder().setName(user).build();
+        HelloReply response = blockingStub.sayHello(request);
+        logger.info("Greeting: " + response.getMessage());
+      } catch (StatusRuntimeException e) {
+        logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+      }
+    } finally {
+      // ManagedChannels use resources like threads and TCP connections. To prevent
+      // leaking these
+      // resources the channel should be shut down when it will no longer be used. If
+      // it may be used
+      // again leave it running.
+      channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
   }
 }
