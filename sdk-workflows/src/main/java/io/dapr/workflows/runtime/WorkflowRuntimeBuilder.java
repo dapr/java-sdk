9bb9c7d60b189e33b1ee88446f7c200cff0943c4
Ryan Lettieri <ryanLettieri@microsoft.com>
2023-12-27 14:19:13 -0700

    Cleaning up linter issues
    Signed-off-by: Ryan Lettieri <ryanLettieri@microsoft.com>

--- sdk-workflows/src/main/java/io/dapr/workflows/runtime/WorkflowRuntimeBuilder.java
+++ sdk-workflows/src/main/java/io/dapr/workflows/runtime/WorkflowRuntimeBuilder.java
@@ -32,47 +32,62 @@ public class WorkflowRuntimeBuilder {
   private ArrayList<String> activities = new ArrayList<String>();
   private static ClientInterceptor WORKFLOW_INTERCEPTOR = new ApiTokenClientInterceptor();
 
+  /**
+   * Constructs the WorkflowRuntimeBuilder.
+   */
   public WorkflowRuntimeBuilder() {
     this.builder = new DurableTaskGrpcWorkerBuilder().grpcChannel(
                           NetworkUtils.buildGrpcManagedChannel(WORKFLOW_INTERCEPTOR));
     this.logger = Logger.getLogger(WorkflowRuntimeBuilder.class.getName());
     this.logLevel = System.getenv("DAPR_LOG_LEVEL");
     if (this.logLevel == null || this.logLevel.isEmpty()) {
-     this.logger.log(Level.INFO, "Setting the Dapr workflow runtime log level to INFO");
-     this.logger.log(Level.INFO, "To change the log level, set the DAPR_LOG_LEVEL environment variable to one of the following: SEVERE, WARNING, INFO, DEBUG, CONFIG, FINE, FINER, FINEST");
-     this.logger.setLevel(Level.INFO); 
-    }
-    else{
+      this.logger.log(Level.INFO, "Setting the Dapr workflow runtime log level to INFO");
+      this.logger.log(Level.INFO, "To change the log level, set the DAPR_LOG_LEVEL environment variable to one of the"
+          +  " following: SEVERE, WARNING, INFO, DEBUG, CONFIG, FINE, FINER, FINEST");
+      this.logger.setLevel(Level.INFO); 
+    } else {
       // try to convert environment variabel to a specific log level.
-      switch (this.logLevel.toUpperCase()){
+      switch (this.logLevel.toUpperCase()) {
         case "SEVERE":
           this.logger.setLevel(Level.SEVERE);
           this.logger.log(Level.INFO, "Setting the Dapr workflow runtime log level to SEVERE");
+          break;
         case "WARNING":
           this.logger.setLevel(Level.WARNING);
           this.logger.log(Level.INFO, "Setting the Dapr workflow runtime log level to WARNING");
+          break;
         case "INFO":
           this.logger.setLevel(Level.INFO);
           this.logger.log(Level.INFO, "Setting the Dapr workflow runtime log level to INFO");
+          break;
         case "DEBUG":
-        // DEBUG is not a Java regonized log level enumeration, so it might make sense to use "CONFIG" as the replacement level and still allow the user to set the variable to DEBUG and we convert it here
+          // DEBUG is not a Java regonized log level enumeration, so it might make sense to use "CONFIG" as the
+          // replacement level and still allow the user to set the variable to DEBUG and we convert it here
           this.logger.setLevel(Level.CONFIG);
           this.logger.log(Level.INFO, "Setting the Dapr workflow runtime log level to DEBUG/CONFIG");
+          break;
         case "CONFIG":
           this.logger.setLevel(Level.CONFIG);
           this.logger.log(Level.INFO, "Setting the Dapr workflow runtime log level to DEBUG/CONFIG");
+          break;
         case "FINE":
           this.logger.setLevel(Level.FINE);
           this.logger.log(Level.INFO, "Setting the Dapr workflow runtime log level to FINE");
+          break;
         case "FINER":
           this.logger.setLevel(Level.FINER);
           this.logger.log(Level.INFO, "Setting the Dapr workflow runtime log level to FINER");
+          break;
         case "FINEST":
           this.logger.setLevel(Level.FINEST);
           this.logger.log(Level.INFO, "Setting the Dapr workflow runtime log level to FINEST");
+          break;
         default:
-          this.logger.log(Level.INFO, "Environment variable: DAPR_LOG_LEVEL was not set to a recognized value. Defaulting log level to INFO");
-          this.logger.log(Level.INFO, "Available levels for the DAPR_LOG_LEVEL are: SEVERE, WARNING, INFO, DEBUG, CONFIG, FINE, FINER, FINEST");
+          this.logger.log(Level.INFO, "Environment variable: DAPR_LOG_LEVEL was not set to a recognized value."
+              + " Defaulting log level to INFO");
+          this.logger.log(Level.INFO, "Available levels for the DAPR_LOG_LEVEL are: SEVERE, WARNING, INFO,"
+              + " DEBUG, CONFIG, FINE, FINER, FINEST");
+          break;
       }
     }
   }
@@ -107,10 +122,11 @@ public class WorkflowRuntimeBuilder {
     this.builder = this.builder.addOrchestration(
         new OrchestratorWrapper<>(clazz)
     );
-    this.logger.log(Level.CONFIG, "Registered Workflow: "+  clazz.getCanonicalName());
+    this.logger.log(Level.CONFIG, "Registered Workflow: " +  clazz.getCanonicalName());
     this.workflows.add(clazz.getCanonicalName());
-    // If possible, attempt to grab the workflow names from the underlying durableTask framework and use those since they are already registered in a hashmap
-    // Potentially need to create 'getter' methods inside the underlying java code. This would also apply for the activities
+    // If possible, attempt to grab the workflow names from the underlying durableTask framework and use those
+    // since they are already registered in a hashmap
+    // Potentially need to create getter methods inside the underlying java code. This would also apply for activities
     // Class<?>[] classarry = this.builder.getClass().getClasses();
 
     return this;
@@ -126,7 +142,7 @@ public class WorkflowRuntimeBuilder {
     this.builder = this.builder.addActivity(
         new ActivityWrapper<>(clazz)
     );
-    this.logger.log(Level.CONFIG, "Registered Activity: "+  clazz.getCanonicalName());
+    this.logger.log(Level.CONFIG, "Registered Activity: " +  clazz.getCanonicalName());
     this.activities.add(clazz.getCanonicalName());
   }
 }
