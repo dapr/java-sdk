a13c12191db5d7ed9814c619ac8223ba449eb2b8
Mukundan Sundararajan <65565396+mukundansundar@users.noreply.github.com>
2024-02-08 12:44:50 +0530

    properly close managed channel in network utils test
    Signed-off-by: Mukundan Sundararajan <65565396+mukundansundar@users.noreply.github.com>

--- sdk/src/test/java/io/dapr/utils/NetworkUtilsTest.java
+++ sdk/src/test/java/io/dapr/utils/NetworkUtilsTest.java
@@ -2,6 +2,7 @@ package io.dapr.utils;
 
 import io.dapr.config.Properties;
 import io.grpc.ManagedChannel;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.Test;
 public class NetworkUtilsTest {
   private final int defaultGrpcPort = 4000;
   private final String defaultSidecarIP = "127.0.0.1";
+
+  private ManagedChannel channel;
   @BeforeEach
   public void setUp() {
     System.setProperty(Properties.GRPC_PORT.getName(), Integer.toString(defaultGrpcPort));
@@ -16,9 +19,17 @@ public class NetworkUtilsTest {
     System.setProperty(Properties.GRPC_ENDPOINT.getName(), "");
   }
 
+  @AfterEach
+  public void tearDown(){
+    if (channel != null && !channel.isShutdown()) {
+      channel.shutdownNow();
+    }
+  }
+
+
   @Test
   public void testBuildGrpcManagedChannel() {
-    ManagedChannel channel = NetworkUtils.buildGrpcManagedChannel();
+    channel = NetworkUtils.buildGrpcManagedChannel();
 
     String expectedAuthority = String.format("%s:%s", defaultSidecarIP, defaultGrpcPort);
     Assertions.assertEquals(expectedAuthority, channel.authority());
@@ -27,7 +38,7 @@ public class NetworkUtilsTest {
   @Test
   public void testBuildGrpcManagedChannel_httpEndpointNoPort() {
     System.setProperty(Properties.GRPC_ENDPOINT.getName(), "http://example.com");
-    ManagedChannel channel = NetworkUtils.buildGrpcManagedChannel();
+    channel = NetworkUtils.buildGrpcManagedChannel();
 
     String expectedAuthority = "example.com:80";
     Assertions.assertEquals(expectedAuthority, channel.authority());
@@ -36,7 +47,7 @@ public class NetworkUtilsTest {
   @Test
   public void testBuildGrpcManagedChannel_httpEndpointWithPort() {
     System.setProperty(Properties.GRPC_ENDPOINT.getName(), "http://example.com:3000");
-    ManagedChannel channel = NetworkUtils.buildGrpcManagedChannel();
+    channel = NetworkUtils.buildGrpcManagedChannel();
 
     String expectedAuthority = "example.com:3000";
     Assertions.assertEquals(expectedAuthority, channel.authority());
@@ -45,7 +56,7 @@ public class NetworkUtilsTest {
   @Test
   public void testBuildGrpcManagedChannel_httpsEndpointNoPort() {
     System.setProperty(Properties.GRPC_ENDPOINT.getName(), "https://example.com");
-    ManagedChannel channel = NetworkUtils.buildGrpcManagedChannel();
+    channel = NetworkUtils.buildGrpcManagedChannel();
 
     String expectedAuthority = "example.com:443";
     Assertions.assertEquals(expectedAuthority, channel.authority());
@@ -54,7 +65,7 @@ public class NetworkUtilsTest {
   @Test
   public void testBuildGrpcManagedChannel_httpsEndpointWithPort() {
     System.setProperty(Properties.GRPC_ENDPOINT.getName(), "https://example.com:3000");
-    ManagedChannel channel = NetworkUtils.buildGrpcManagedChannel();
+    channel = NetworkUtils.buildGrpcManagedChannel();
 
     String expectedAuthority = "example.com:3000";
     Assertions.assertEquals(expectedAuthority, channel.authority());
