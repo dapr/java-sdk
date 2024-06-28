ff6895f926ff2a0b50d407408ac3e10f52f35c1f
Artur Ciocanu <ciocanu@adobe.com>
2024-06-28 10:12:16 +0300

    Upgrading OTEL to 1.39.0
    Signed-off-by: Artur Ciocanu <ciocanu@adobe.com>

--- sdk-tests/src/test/java/io/dapr/it/tracing/grpc/TracingIT.java
+++ sdk-tests/src/test/java/io/dapr/it/tracing/grpc/TracingIT.java
@@ -9,9 +9,9 @@ import io.dapr.it.DaprRun;
 import io.dapr.it.tracing.Validation;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Scope;
-import io.opentelemetry.sdk.OpenTelemetrySdk;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -37,28 +37,27 @@ public class TracingIT extends BaseIT {
           AppRun.AppProtocol.GRPC,  // appProtocol
           60000);
 
-        daprRun.waitForAppHealth(10000);
+        daprRun.waitForAppHealth(60000);
     }
 
     @Test
     public void testInvoke() throws Exception {
-        final OpenTelemetry openTelemetry = createOpenTelemetry("service over grpc");
-        final Tracer tracer = openTelemetry.getTracer("grpc integration test tracer");
-
-        final String spanName = UUID.randomUUID().toString();
-        Span span = tracer.spanBuilder(spanName).setSpanKind(Span.Kind.CLIENT).startSpan();
+        OpenTelemetry openTelemetry = createOpenTelemetry("service over grpc");
+        Tracer tracer = openTelemetry.getTracer("grpc integration test tracer");
+        String spanName = UUID.randomUUID().toString();
+        Span span = tracer.spanBuilder(spanName).setSpanKind(SpanKind.CLIENT).startSpan();
 
         try (DaprClient client = new DaprClientBuilder().build()) {
             client.waitForSidecar(10000).block();
             try (Scope scope = span.makeCurrent()) {
                 SleepRequest req = SleepRequest.newBuilder().setSeconds(1).build();
                 client.invokeMethod(daprRun.getAppName(), "sleepOverGRPC", req.toByteArray(), HttpExtension.POST)
-                    .contextWrite(getReactorContext())
+                    .contextWrite(getReactorContext(openTelemetry))
                     .block();
             }
         }
+
         span.end();
-        OpenTelemetrySdk.getGlobalTracerManagement().shutdown();
 
         Validation.validate(spanName, "calllocal/tracingitgrpc-service/sleepovergrpc");
     }
