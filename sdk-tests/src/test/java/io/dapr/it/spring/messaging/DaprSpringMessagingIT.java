529db5aef012d6527507507b7a676dbca25372e0
Artur Ciocanu <ciocanu@adobe.com>
2024-09-25 20:45:14 +0300

    Increase the sleep for messaging test
    Signed-off-by: Artur Ciocanu <ciocanu@adobe.com>

--- sdk-tests/src/test/java/io/dapr/it/spring/messaging/DaprSpringMessagingIT.java
+++ sdk-tests/src/test/java/io/dapr/it/spring/messaging/DaprSpringMessagingIT.java
@@ -91,7 +91,7 @@ public class DaprSpringMessagingIT {
     }
 
     // Wait for the messages to arrive
-    Thread.sleep(1000);
+    Thread.sleep(10000);
 
     List<CloudEvent<String>> events = testRestController.getEvents();
 
