5454194bdd6617d394342a9117869a3a24ddef76
salaboy <Salaboy@gmail.com>
2024-12-04 16:01:25 +0000

    evaluating annotations
    Signed-off-by: salaboy <Salaboy@gmail.com>

---
+++ dapr-spring/dapr-spring-workflows/src/main/java/io/dapr/spring/workflows/Operation.java
@@ -0,0 +1,12 @@
+package io.dapr.spring.workflows;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Operation {
+  String name() default "";
+}
