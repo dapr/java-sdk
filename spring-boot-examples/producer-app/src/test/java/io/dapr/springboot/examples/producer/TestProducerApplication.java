77f1e9c2b88902272c9ec048ee1cb9944eecdd08
salaboy <Salaboy@gmail.com>
2025-02-13 11:01:33 +0900

    removing line break
    Signed-off-by: salaboy <Salaboy@gmail.com>

--- spring-boot-examples/producer-app/src/test/java/io/dapr/springboot/examples/producer/TestProducerApplication.java
+++ spring-boot-examples/producer-app/src/test/java/io/dapr/springboot/examples/producer/TestProducerApplication.java
@@ -9,8 +9,7 @@ public class TestProducerApplication {
 
   public static void main(String[] args) {
 
-    SpringApplication
-            .from(ProducerApplication::main)
+    SpringApplication.from(ProducerApplication::main)
             .with(DaprTestContainersConfig.class)
             .run(args);
     org.testcontainers.Testcontainers.exposeHostPorts(8080);
