3b55b4c7a029675678c81853009a4134ad7d2092
lony2003 <zhangke200377@outlook.com>
2025-03-05 09:48:06 +0800

    style(springboot cloudconfig): update file header
    Signed-off-by: lony2003 <zhangke200377@outlook.com>

--- dapr-spring/dapr-spring-cloudconfig/src/main/java/io/dapr/spring/boot/cloudconfig/autoconfigure/DaprCloudConfigAutoConfiguration.java
+++ dapr-spring/dapr-spring-cloudconfig/src/main/java/io/dapr/spring/boot/cloudconfig/autoconfigure/DaprCloudConfigAutoConfiguration.java
@@ -1,18 +1,15 @@
 /*
- * Copyright (c) 2016-2024 Team Fangkehou
- *
+ * Copyright 2025 The Dapr Authors
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+limitations under the License.
+*/
 
 package io.dapr.spring.boot.cloudconfig.autoconfigure;
 
@@ -26,7 +23,7 @@ import io.dapr.client.DaprClient;
 
 @Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties(DaprCloudConfigProperties.class)
-@ConditionalOnProperty(name = "dapr.secretstore.enabled", matchIfMissing = true)
+@ConditionalOnProperty(name = DaprCloudConfigProperties.PROPERTY_PREFIX + ".enabled", matchIfMissing = true)
 @ConditionalOnClass(DaprClient.class)
 public class DaprCloudConfigAutoConfiguration {
 
