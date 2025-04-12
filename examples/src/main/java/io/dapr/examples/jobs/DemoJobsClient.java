17429d645d8ea8f23d98724272fea12696e4ceda
siri-varma <siri.varma@outlook.com>
2025-04-12 09:42:25 +0530

    Add examples
    Signed-off-by: siri-varma <siri.varma@outlook.com>

---
+++ examples/src/main/java/io/dapr/examples/jobs/DemoJobsClient.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2021 The Dapr Authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package io.dapr.examples.jobs;
+
+import io.dapr.client.DaprClientBuilder;
+import io.dapr.client.DaprPreviewClient;
+import io.dapr.client.domain.GetJobRequest;
+import io.dapr.client.domain.GetJobResponse;
+import io.dapr.client.domain.JobSchedule;
+import io.dapr.client.domain.ScheduleJobRequest;
+import io.dapr.config.Properties;
+import io.dapr.config.Property;
+
+import java.util.Map;
+
+public class DemoJobsClient {
+
+  /**
+   * The main method of this app to register and fetch jobs.
+   */
+  public static void main(String[] args) throws Exception {
+    Map<Property<?>, String> overrides = Map.of(
+        Properties.HTTP_PORT, "3500",
+        Properties.GRPC_PORT, "51439"
+    );
+
+    try (DaprPreviewClient client = new DaprClientBuilder().withPropertyOverrides(overrides).buildPreviewClient()) {
+
+      // Schedule a job.
+      ScheduleJobRequest scheduleJobRequest = new ScheduleJobRequest("dapr-job-1",
+          JobSchedule.fromString("* * * * * *")).setData("Hello World!".getBytes());
+      client.scheduleJob(scheduleJobRequest).block();
+
+      // Get a job.
+      GetJobResponse getJobResponse = client.getJob(new GetJobRequest("dapr-job-1")).block();
+    }
+  }
+}
