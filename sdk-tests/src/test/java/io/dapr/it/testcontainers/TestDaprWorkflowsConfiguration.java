f724e48aa145a3183b4338024314ad36e476a544
Javier Aliaga <javier@diagrid.io>
2025-05-28 20:14:59 +0200

    Revert "chore: New task execution task id test (#1352)" (#1389)
    This reverts commit 949584f69f4bc78ca59abe6ad000f1f2ace21768.
    
    Signed-off-by: Javier Aliaga <javier@diagrid.io>
    Signed-off-by: sirivarma <siri.varma@outlook.com>

--- sdk-tests/src/test/java/io/dapr/it/testcontainers/TestDaprWorkflowsConfiguration.java
+++ sdk-tests/src/test/java/io/dapr/it/testcontainers/TestDaprWorkflowsConfiguration.java
@@ -56,12 +56,9 @@ public class TestDaprWorkflowsConfiguration {
     WorkflowRuntimeBuilder builder = new WorkflowRuntimeBuilder(new Properties(overrides));
 
     builder.registerWorkflow(TestWorkflow.class);
-    builder.registerWorkflow(TestExecutionKeysWorkflow.class);
     builder.registerActivity(FirstActivity.class);
     builder.registerActivity(SecondActivity.class);
-    builder.registerActivity(TaskExecutionKeyActivity.class);
-    
-    
+
     return builder;
   }
 }
