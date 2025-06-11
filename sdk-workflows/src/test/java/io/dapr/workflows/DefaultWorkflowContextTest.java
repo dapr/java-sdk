cbfbbbfcbdae1218e1fb816f2db8d9f71396a7f2
Cassandra Coyle <cassie@diagrid.io>
2025-06-11 14:06:01 -0500

    add missing import
    Signed-off-by: Cassandra Coyle <cassie@diagrid.io>

--- sdk-workflows/src/test/java/io/dapr/workflows/DefaultWorkflowContextTest.java
+++ sdk-workflows/src/test/java/io/dapr/workflows/DefaultWorkflowContextTest.java
@@ -38,6 +38,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
