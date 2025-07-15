9257432d9fa08bd5769b46ba93fb287651045cfe
salaboy <Salaboy@gmail.com>
2025-07-15 15:53:31 +0100

    adding tests for coverage
    Signed-off-by: salaboy <Salaboy@gmail.com>

--- testcontainers-dapr/src/test/java/io/dapr/testcontainers/DaprContainerTest.java
+++ testcontainers-dapr/src/test/java/io/dapr/testcontainers/DaprContainerTest.java
@@ -26,4 +26,21 @@ public class DaprContainerTest {
     assertEquals("custom/scheduler:1.15.4", dapr.getSchedulerDockerImageName().asCanonicalNameString());
 
   }
+
+  @Test
+  public void schedulerAndPlacementCustomImagesStringTest() {
+
+    DaprContainer dapr = new DaprContainer(DAPR_RUNTIME_IMAGE_TAG)
+            .withAppName("dapr-app")
+            .withSchedulerImage("daprio/scheduler:1.15.4")
+            .withPlacementImage("daprio/placement:1.15.4")
+            .withAppPort(8081)
+            .withDaprLogLevel(DaprLogLevel.DEBUG)
+            .withAppChannelAddress("host.testcontainers.internal");
+
+
+    assertEquals("daprio/placement:1.15.4", dapr.getPlacementDockerImageName().asCanonicalNameString());
+    assertEquals("daprio/scheduler:1.15.4", dapr.getSchedulerDockerImageName().asCanonicalNameString());
+
+  }
 }
