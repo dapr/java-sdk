4256a203eaaff0172ec34b2ac4319f657d2dd305
salaboy <Salaboy@gmail.com>
2025-07-18 08:27:26 +0100

    upgrading to 1.15.7
    Signed-off-by: salaboy <Salaboy@gmail.com>

--- testcontainers-dapr/src/test/java/io/dapr/testcontainers/DaprContainerTest.java
+++ testcontainers-dapr/src/test/java/io/dapr/testcontainers/DaprContainerTest.java
@@ -13,17 +13,17 @@ public class DaprContainerTest {
 
     DaprContainer dapr = new DaprContainer(DAPR_RUNTIME_IMAGE_TAG)
             .withAppName("dapr-app")
-            .withSchedulerImage(DockerImageName.parse("custom/scheduler:1.15.4")
-                    .asCompatibleSubstituteFor("daprio/scheduler:1.15.4"))
-            .withPlacementImage(DockerImageName.parse("custom/placement:1.15.4")
-                    .asCompatibleSubstituteFor("daprio/placement:1.15.4"))
+            .withSchedulerImage(DockerImageName.parse("custom/scheduler:1.15.7")
+                    .asCompatibleSubstituteFor("daprio/scheduler:1.15.7"))
+            .withPlacementImage(DockerImageName.parse("custom/placement:1.15.7")
+                    .asCompatibleSubstituteFor("daprio/placement:1.15.7"))
             .withAppPort(8081)
             .withDaprLogLevel(DaprLogLevel.DEBUG)
             .withAppChannelAddress("host.testcontainers.internal");
 
 
-    assertEquals("custom/placement:1.15.4", dapr.getPlacementDockerImageName().asCanonicalNameString());
-    assertEquals("custom/scheduler:1.15.4", dapr.getSchedulerDockerImageName().asCanonicalNameString());
+    assertEquals("custom/placement:1.15.7", dapr.getPlacementDockerImageName().asCanonicalNameString());
+    assertEquals("custom/scheduler:1.15.7", dapr.getSchedulerDockerImageName().asCanonicalNameString());
 
   }
 
@@ -32,15 +32,15 @@ public class DaprContainerTest {
 
     DaprContainer dapr = new DaprContainer(DAPR_RUNTIME_IMAGE_TAG)
             .withAppName("dapr-app")
-            .withSchedulerImage("daprio/scheduler:1.15.4")
-            .withPlacementImage("daprio/placement:1.15.4")
+            .withSchedulerImage("daprio/scheduler:1.15.7")
+            .withPlacementImage("daprio/placement:1.15.7")
             .withAppPort(8081)
             .withDaprLogLevel(DaprLogLevel.DEBUG)
             .withAppChannelAddress("host.testcontainers.internal");
 
 
-    assertEquals("daprio/placement:1.15.4", dapr.getPlacementDockerImageName().asCanonicalNameString());
-    assertEquals("daprio/scheduler:1.15.4", dapr.getSchedulerDockerImageName().asCanonicalNameString());
+    assertEquals("daprio/placement:1.15.7", dapr.getPlacementDockerImageName().asCanonicalNameString());
+    assertEquals("daprio/scheduler:1.15.7", dapr.getSchedulerDockerImageName().asCanonicalNameString());
 
   }
 }
