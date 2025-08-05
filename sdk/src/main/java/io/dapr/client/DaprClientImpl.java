7c3afb3baa3a562ad7722f23f3e048e5668cd916
Siri Varma Vegiraju <siri.varma@outlook.com>
2025-08-05 11:00:57 -0700

    Add Failure Policy for Jobs SDK (#1448)
    * Update CONTRIBUTING.md
    
    Signed-off-by: Siri Varma Vegiraju <siri.varma@outlook.com>
    
    * Add failrue policy
    
    Signed-off-by: sirivarma <siri.varma@outlook.com>
    
    * Add tests
    
    Signed-off-by: sirivarma <siri.varma@outlook.com>
    
    * Add Tests
    
    Signed-off-by: sirivarma <siri.varma@outlook.com>
    
    * Upgrading to 1.15.7 (#1458)
    
    * upgrading to 1.15.7
    
    Signed-off-by: salaboy <Salaboy@gmail.com>
    
    * using DAPR VERSION
    
    Signed-off-by: salaboy <Salaboy@gmail.com>
    
    ---------
    
    Signed-off-by: salaboy <Salaboy@gmail.com>
    Signed-off-by: siri-varma <siri.varma@outlook.com>
    
    * Rename classes
    
    Signed-off-by: siri-varma <siri.varma@outlook.com>
    
    * add rc
    
    Signed-off-by: sirivarma <siri.varma@outlook.com>
    
    * fix checkstyle
    
    Signed-off-by: sirivarma <siri.varma@outlook.com>
    
    * Fix things
    
    Signed-off-by: sirivarma <siri.varma@outlook.com>
    
    * Test latest
    
    Signed-off-by: sirivarma <siri.varma@outlook.com>
    
    * fix checkstyle
    
    Signed-off-by: sirivarma <siri.varma@outlook.com>
    
    * Address comments
    
    Signed-off-by: sirivarma <siri.varma@outlook.com>
    
    * Address comments
    
    Signed-off-by: sirivarma <siri.varma@outlook.com>
    
    ---------
    
    Signed-off-by: Siri Varma Vegiraju <siri.varma@outlook.com>
    Signed-off-by: sirivarma <siri.varma@outlook.com>
    Signed-off-by: salaboy <Salaboy@gmail.com>
    Signed-off-by: siri-varma <siri.varma@outlook.com>
    Co-authored-by: artur-ciocanu <artur.ciocanu@gmail.com>
    Co-authored-by: salaboy <Salaboy@gmail.com>
    Signed-off-by: sirivarma <siri.varma@outlook.com>

--- sdk/src/main/java/io/dapr/client/DaprClientImpl.java
+++ sdk/src/main/java/io/dapr/client/DaprClientImpl.java
@@ -31,6 +31,7 @@ import io.dapr.client.domain.CloudEvent;
 import io.dapr.client.domain.ComponentMetadata;
 import io.dapr.client.domain.ConfigurationItem;
 import io.dapr.client.domain.ConversationToolsFunction;
+import io.dapr.client.domain.ConstantFailurePolicy;
 import io.dapr.client.domain.ConversationInput;
 import io.dapr.client.domain.ConversationInputAlpha2;
 import io.dapr.client.domain.ConversationMessage;
@@ -49,7 +50,10 @@ import io.dapr.client.domain.ConversationTools;
 import io.dapr.client.domain.DaprMetadata;
 import io.dapr.client.domain.DeleteJobRequest;
 import io.dapr.client.domain.DeleteStateRequest;
+import io.dapr.client.domain.DropFailurePolicy;
 import io.dapr.client.domain.ExecuteStateTransactionRequest;
+import io.dapr.client.domain.FailurePolicy;
+import io.dapr.client.domain.FailurePolicyType;
 import io.dapr.client.domain.GetBulkSecretRequest;
 import io.dapr.client.domain.GetBulkStateRequest;
 import io.dapr.client.domain.GetConfigurationRequest;
@@ -122,6 +126,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -1353,6 +1358,10 @@ public class DaprClientImpl extends AbstractDaprClient {
         scheduleJobRequestBuilder.setDueTime(iso8601Formatter.format(scheduleJobRequest.getDueTime()));
       }
 
+      if (scheduleJobRequest.getFailurePolicy() != null) {
+        scheduleJobRequestBuilder.setFailurePolicy(getJobFailurePolicy(scheduleJobRequest.getFailurePolicy()));
+      }
+
       scheduleJobRequestBuilder.setOverwrite(scheduleJobRequest.getOverwrite());
 
       Mono<DaprProtos.ScheduleJobResponse> scheduleJobResponseMono =
@@ -1397,6 +1406,10 @@ public class DaprClientImpl extends AbstractDaprClient {
           getJobResponse = new GetJobResponse(job.getName(), Instant.parse(job.getDueTime()));
         }
 
+        if (job.hasFailurePolicy()) {
+          getJobResponse.setFailurePolicy(getJobFailurePolicy(job.getFailurePolicy()));
+        }
+
         return getJobResponse
             .setTtl(job.hasTtl() ? Instant.parse(job.getTtl()) : null)
             .setData(job.hasData() ? job.getData().getValue().toByteArray() : null)
@@ -1405,6 +1418,53 @@ public class DaprClientImpl extends AbstractDaprClient {
     } catch (Exception ex) {
       return DaprException.wrapMono(ex);
     }
+  }
+
+  private FailurePolicy getJobFailurePolicy(CommonProtos.JobFailurePolicy jobFailurePolicy) {
+    if (jobFailurePolicy.hasDrop()) {
+      return new DropFailurePolicy();
+    }
+
+    CommonProtos.JobFailurePolicyConstant jobFailurePolicyConstant = jobFailurePolicy.getConstant();
+    if (jobFailurePolicyConstant.hasInterval() && jobFailurePolicyConstant.hasMaxRetries()) {
+      return new ConstantFailurePolicy(jobFailurePolicyConstant.getMaxRetries())
+              .setDurationBetweenRetries(Duration.of(jobFailurePolicyConstant.getInterval().getNanos(),
+                  ChronoUnit.NANOS));
+    }
+
+    if (jobFailurePolicyConstant.hasMaxRetries()) {
+      return new ConstantFailurePolicy(jobFailurePolicyConstant.getMaxRetries());
+    }
+
+    return new ConstantFailurePolicy(
+        Duration.of(jobFailurePolicyConstant.getInterval().getNanos(),
+            ChronoUnit.NANOS));
+  }
+
+  private CommonProtos.JobFailurePolicy getJobFailurePolicy(FailurePolicy failurePolicy) {
+    CommonProtos.JobFailurePolicy.Builder jobFailurePolicyBuilder = CommonProtos.JobFailurePolicy.newBuilder();
+
+    if (failurePolicy.getFailurePolicyType() == FailurePolicyType.DROP) {
+      jobFailurePolicyBuilder.setDrop(CommonProtos.JobFailurePolicyDrop.newBuilder().build());
+      return jobFailurePolicyBuilder.build();
+    }
+
+    CommonProtos.JobFailurePolicyConstant.Builder constantPolicyBuilder =
+        CommonProtos.JobFailurePolicyConstant.newBuilder();
+    ConstantFailurePolicy jobConstantFailurePolicy = (ConstantFailurePolicy)failurePolicy;
+
+    if (jobConstantFailurePolicy.getMaxRetries() != null) {
+      constantPolicyBuilder.setMaxRetries(jobConstantFailurePolicy.getMaxRetries());
+    }
+
+    if (jobConstantFailurePolicy.getDurationBetweenRetries() != null) {
+      constantPolicyBuilder.setInterval(com.google.protobuf.Duration.newBuilder()
+          .setNanos(jobConstantFailurePolicy.getDurationBetweenRetries().getNano()).build());
+    }
+
+    jobFailurePolicyBuilder.setConstant(constantPolicyBuilder.build());
+
+    return jobFailurePolicyBuilder.build();
   }
 
   /**
