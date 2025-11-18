b394832fca339eae8aed1debfb3b1d850208ea10
salaboy <Salaboy@gmail.com>
2025-11-18 11:45:18 +0000

    fix style
    Signed-off-by: salaboy <Salaboy@gmail.com>

--- sdk-workflows/src/main/java/io/dapr/workflows/client/DaprWorkflowClient.java
+++ sdk-workflows/src/main/java/io/dapr/workflows/client/DaprWorkflowClient.java
@@ -239,7 +239,7 @@ public class DaprWorkflowClient implements AutoCloseable {
     try {
       this.innerClient.resumeInstance(workflowInstanceId, reason);
     } finally {
-        span.end();
+      span.end();
     }
   }
 
