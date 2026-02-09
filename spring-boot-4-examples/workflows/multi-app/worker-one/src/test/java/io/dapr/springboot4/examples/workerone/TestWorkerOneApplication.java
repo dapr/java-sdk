b80720118ed09d74683eeb25ef7a78df6307c593
salaboy <Salaboy@gmail.com>
2026-02-09 17:36:05 +0000

    adding spring-data-6 and spring-boot-4-starters
    Signed-off-by: salaboy <Salaboy@gmail.com>

---
+++ spring-boot-4-examples/workflows/multi-app/worker-one/src/test/java/io/dapr/springboot4/examples/workerone/TestWorkerOneApplication.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2025 The Dapr Authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package io.dapr.springboot4.examples.workerone;
+
+import io.dapr.springboot.examples.workerone.WorkerOneApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TestWorkerOneApplication {
+
+  public static void main(String[] args) {
+
+    SpringApplication.from(WorkerOneApplication::main)
+            .with(DaprTestContainersConfig.class)
+            .run(args);
+    org.testcontainers.Testcontainers.exposeHostPorts(8081);
+  }
+
+}
