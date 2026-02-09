519e0c7c35efc13c52a3cb8eef7f657a5695bb2d
salaboy <Salaboy@gmail.com>
2026-02-09 18:41:06 +0000

    adding spring-boot-4-sdk-tests to split dependencies
    Signed-off-by: salaboy <Salaboy@gmail.com>

---
+++ spring-boot-4-sdk-tests/src/test/java/io/dapr/it/springboot4/testcontainers/core/DaprSchedulerContainerIT.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2024 The Dapr Authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package io.dapr.it.springboot4.testcontainers.core;
+
+import io.dapr.testcontainers.DaprSchedulerContainer;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static io.dapr.it.springboot4.testcontainers.ContainerConstants.DAPR_SCHEDULER_IMAGE_TAG;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Testcontainers
+@Tag("testcontainers")
+public class DaprSchedulerContainerIT {
+
+  @Container
+  private static final DaprSchedulerContainer SCHEDULER_CONTAINER =
+          new DaprSchedulerContainer(DAPR_SCHEDULER_IMAGE_TAG);
+
+  @Test
+  public void testDaprSchedulerContainerDefaults() {
+    assertEquals(51005, SCHEDULER_CONTAINER.getPort(), "The default port is not set");
+  }
+}
