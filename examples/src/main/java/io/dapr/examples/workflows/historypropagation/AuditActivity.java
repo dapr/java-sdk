d68b814bfe5452e597380692708f15453bace94d
Siri Varma Vegiraju <s_vegiraju@apple.com>
2026-05-18 08:16:55 -0700

    Modify readme
    Signed-off-by: Siri Varma Vegiraju <s_vegiraju@apple.com>

---
+++ examples/src/main/java/io/dapr/examples/workflows/historypropagation/AuditActivity.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2025 The Dapr Authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package io.dapr.examples.workflows.historypropagation;
+
+import io.dapr.durabletask.PropagatedHistory;
+import io.dapr.durabletask.PropagatedHistoryChunk;
+import io.dapr.workflows.WorkflowActivity;
+import io.dapr.workflows.WorkflowActivityContext;
+
+import java.util.Optional;
+
+/**
+ * Activity that audits its caller's execution history.
+ *
+ * <p>Invoked with OWN_HISTORY scope - sees only the immediate caller's events
+ * (a trust boundary; no grandparent history is propagated through).</p>
+ */
+public class AuditActivity implements WorkflowActivity {
+  @Override
+  public Object run(WorkflowActivityContext ctx) {
+    ctx.getLogger().info("Auditing payment for: " + ctx.getInput(String.class));
+
+    Optional<PropagatedHistory> historyOpt = ctx.getPropagatedHistory();
+    if (historyOpt.isPresent()) {
+      PropagatedHistory history = historyOpt.get();
+      ctx.getLogger().info("Audit received history (scope=" + history.getScope()
+          + ", chunks=" + history.getWorkflows().size() + ")");
+      for (PropagatedHistoryChunk chunk : history.getWorkflows()) {
+        ctx.getLogger().info("  chunk: workflow=" + chunk.getWorkflowName()
+            + " instance=" + chunk.getInstanceId() + " events=" + chunk.getEventCount());
+      }
+    } else {
+      ctx.getLogger().info("Audit received no propagated history");
+    }
+
+    return "audited";
+  }
+}
---
+++ examples/src/main/java/io/dapr/examples/workflows/historypropagation/multiapp/App1Worker.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2025 The Dapr Authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package io.dapr.examples.workflows.historypropagation.multiapp;
+
+import io.dapr.workflows.runtime.WorkflowRuntime;
+import io.dapr.workflows.runtime.WorkflowRuntimeBuilder;
+
+/**
+ * App1 worker - registers only App1Workflow.
+ */
+public class App1Worker {
+
+  public static void main(String[] args) throws Exception {
+    WorkflowRuntimeBuilder builder = new WorkflowRuntimeBuilder()
+        .registerWorkflow(App1Workflow.class);
+
+    try (WorkflowRuntime runtime = builder.build()) {
+      System.out.println("App1Worker started - registered App1Workflow only");
+      runtime.start();
+    }
+  }
+}
