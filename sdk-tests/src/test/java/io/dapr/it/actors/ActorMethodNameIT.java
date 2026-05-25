ff1defa785e2f68b2f194a38ff75b074350f81e0
Siri Varma Vegiraju <siri.varma@outlook.com>
2026-05-25 10:37:27 -0700

    Migrate ActorMethodNameIT to Testcontainers
    Per-class @BeforeAll using startAppAndAttach with MyActorService.
    Both proxy builders share the per-class actorClient.
    
    Signed-off-by: Siri Varma Vegiraju <siri.varma@outlook.com>

--- sdk-tests/src/test/java/io/dapr/it/actors/ActorMethodNameIT.java
+++ sdk-tests/src/test/java/io/dapr/it/actors/ActorMethodNameIT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 The Dapr Authors
+ * Copyright 2025 The Dapr Authors
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,11 +14,15 @@ limitations under the License.
 package io.dapr.it.actors;
 
 import io.dapr.actors.ActorId;
+import io.dapr.actors.client.ActorClient;
 import io.dapr.actors.client.ActorProxy;
 import io.dapr.actors.client.ActorProxyBuilder;
-import io.dapr.it.BaseIT;
+import io.dapr.it.AppRun;
 import io.dapr.it.actors.app.MyActor;
 import io.dapr.it.actors.app.MyActorService;
+import io.dapr.it.containers.BaseContainerIT;
+import io.dapr.testcontainers.DaprContainer;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,23 +30,38 @@ import org.slf4j.LoggerFactory;
 import static io.dapr.it.Retry.callWithRetry;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ActorMethodNameIT extends BaseIT {
+public class ActorMethodNameIT extends BaseContainerIT {
 
   private static Logger logger = LoggerFactory.getLogger(ActorMethodNameIT.class);
 
-  @Test
-  public void actorMethodNameChange() throws Exception {
-    // The call below will fail if service cannot start successfully.
-    var run = startDaprApp(
-        ActorMethodNameIT.class.getSimpleName(),
-        MyActorService.SUCCESS_MESSAGE,
-        MyActorService.class,
-        true,
-        60000);
+  private static DaprContainer dapr;
+  private static AppRun app;
+  private static ActorClient actorClient;
 
+  @BeforeAll
+  public static void start() throws Exception {
+    var pair = startAppAndAttach(
+        "actor-method-name-it",
+        MyActorService.class,
+        AppRun.AppProtocol.HTTP,
+        appPort -> {
+          DaprContainer d = daprBuilder("actor-method-name-it")
+              .withAppPort(appPort)
+              .withAppChannelAddress("host.testcontainers.internal")
+              .withComponent(redisStateStore(STATE_STORE_NAME));
+          d.start();
+          return d;
+        });
+    dapr = pair.dapr();
+    app = pair.app();
+    actorClient = newActorClient(dapr);
+  }
+
+  @Test
+  public void actorMethodNameChange() {
     logger.debug("Creating proxy builder");
     ActorProxyBuilder<MyActor> proxyBuilder =
-        new ActorProxyBuilder("MyActorTest", MyActor.class, deferClose(run.newActorClient()));
+        new ActorProxyBuilder("MyActorTest", MyActor.class, actorClient);
     logger.debug("Creating actorId");
     ActorId actorId1 = new ActorId("1");
     logger.debug("Building proxy");
@@ -57,7 +76,7 @@ public class ActorMethodNameIT extends BaseIT {
 
     logger.debug("Creating proxy builder 2");
     ActorProxyBuilder<ActorProxy> proxyBuilder2 =
-        new ActorProxyBuilder("MyActorTest", ActorProxy.class, deferClose(run.newActorClient()));
+        new ActorProxyBuilder("MyActorTest", ActorProxy.class, actorClient);
     logger.debug("Building proxy 2");
     ActorProxy proxy2 = proxyBuilder2.build(actorId1);
 
@@ -67,6 +86,5 @@ public class ActorMethodNameIT extends BaseIT {
       logger.debug("asserting true response 2: [" + response + "]");
       assertTrue(response);
     }, 60000);
-
   }
 }
